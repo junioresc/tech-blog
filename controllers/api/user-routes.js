b45e90d4b78f6d25d56d0995ab2590567cd40dc8
Junioresc <junioresc1092@gmail.com>
2020-09-06 18:46:18 -0400

    fixed the post model to not limit the size of the post description

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -49,6 +49,8 @@ router.post(`/`, (req, res) => {
                 req.session.user_id = dbUserData.id;
                 req.session.username = dbUserData.username;
                 req.session.loggedIn = true;
+
+                res.json(dbUserData);
             });
         })
         .catch(err => {
--- models/Post.js
+++ models/Post.js
@@ -16,7 +16,7 @@ Post.init(
             allowNull: false
         },
         post_description: {
-            type: DataTypes.STRING,
+            type: DataTypes.TEXT,
             allowNull: false
         },
         user_id: {

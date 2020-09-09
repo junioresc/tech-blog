fa337625adf1badce8c5568121b64fc95c22aeaf
Junioresc <junioresc1092@gmail.com>
2020-09-09 13:13:47 -0400

    page now reloads when you sign up

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -18,6 +18,7 @@ async function signupFormHandler(event) {
 
         if(response.ok) {
             console.log(`success`);
+            document.location.reload();
         } else {
             alert(response.statusText);
         }
